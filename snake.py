9df31274b56ed14b137eb2ec77139f800de13c55
mgallimore88 <mgallimore88@gmail.com>
2021-12-04 16:27:24 -0800

    adds wraparound

--- snake.py
+++ snake.py
@@ -1,32 +1,29 @@
 from p5_code.vector import Vector
 import numpy as np
 
+
 class Snake:
     def __init__(self):
         self.res = 20
-        self.head = Vector(0,0)
-        self.velocity = Vector(0,0)
-        self.size = Vector(self.res,self.res)
+        self.head = Vector(0, 0)
+        self.velocity = Vector(0, 0)
+        self.size = Vector(self.res, self.res)
         self.length = 3
-        self.body = np.array([[Vector(0,0)], [Vector(0,0)], [Vector(0,0)]])
-
+        self.body = np.array([[Vector(0, 0)], [Vector(0, 0)], [Vector(0, 0)]])
 
     def grow(self):
         self.length += 1
         self.body = np.append(self.body, self.body[-1].copy())
-        self.body = np.reshape(self.body, (self.length,1, 3))
+        self.body = np.reshape(self.body, (self.length, 1, 3))
         print(self.body)
 
-
-    
-
-       
     def move(self):
 
         for i in reversed(range(len(self.body))):
-            self.body[i] = self.body[i-1]
+            self.body[i] = self.body[i - 1]
         self.body[0] = self.head
         self.head += self.velocity
+
 
         def wraparound():
             if self.head.x > self.res:
@@ -41,11 +38,10 @@ class Snake:
 
     def direction(self, direction):
         if direction == "UP":
-            self.velocity = Vector(0,-1)
+            self.velocity = Vector(0, -1)
         if direction == "DOWN":
-            self.velocity = Vector(0,1)
+            self.velocity = Vector(0, 1)
         if direction == "LEFT":
-            self.velocity = Vector(-1,0)
+            self.velocity = Vector(-1, 0)
         if direction == "RIGHT":
-            self.velocity = Vector(1,0)
-
+            self.velocity = Vector(1, 0)
